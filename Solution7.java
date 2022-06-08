0c8ffbe32a85ea6a22f60b3c9998effa51c844ea
gauravkumar1207 <104349769+gauravkumar1207@users.noreply.github.com>
2022-06-08 23:25:46 +0530

    Add files via upload

---
+++ Solution7.java
@@ -0,0 +1,31 @@
+package com.assignment4;
+
+import java.util.Scanner;
+public class Solution7 {
+ 
+  public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+ 
+  System.out.print("Enter 1st number: ");
+  int num1 = in.nextInt();
+ 
+  System.out.print("Enter 2nd number: ");
+  int num2 = in.nextInt();
+ 
+  System.out.print("Enter 3rd number: ");
+  int num3 = in.nextInt();
+ 
+ 
+  if (num1 > num2)
+   if (num1 > num3)
+    System.out.println("The greatest: " + num1);
+ 
+  if (num2 > num1)
+   if (num2 > num3)
+    System.out.println("The greatest: " + num2);
+ 
+  if (num3 > num1)
+   if (num3 > num2)
+    System.out.println("The greatest numbers is : " + num3);
+ }
+}
